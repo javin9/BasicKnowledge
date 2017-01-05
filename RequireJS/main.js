b60442b871cf769fe6f8e896dd917e43b61197b4
IICupid <liujianwei1213@163.com>
2017-01-05 18:03:03 +0800

    2
    2

---
+++ RequireJS/main.js
@@ -0,0 +1,19 @@
+
+require.config({
+//	baseUrl: "js",
+//	paths:{
+//		"jquery":"js/jquery-2.1.1",
+//		"test":"js/test",
+//		'underscore':'js/underscore'
+//	}
+	//配置不兼容的模块。具体来说，每个模块要定义（1）exports值（输出的变量名），表明这个模块外部调用时的名称；（2）deps数组，表明该模块的依赖性。
+//	shim:{
+//		'underscore':{
+//			deps:['jquery'],
+//			export:'_'
+//		}
+//	}
+});
+
+
+
