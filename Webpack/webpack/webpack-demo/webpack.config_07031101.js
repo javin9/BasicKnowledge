5399a644952e1dbdcc0f96b2cf33f01fe555c367
liujianwei <liujianwei@zuoyebang.com>
2018-09-03 09:59:19 +0800

    demo测试

--- Webpack/webpack/webpack-demo/webpack.config_07031101.js
+++ Webpack/webpack/webpack-demo/webpack.config_07031101.js
@@ -1,26 +1,26 @@
-var path=require('path');
-var htmlWebpackPlugin=require('html-webpack-plugin');//commonJS的写法
-module.exports={
-//	entry: ['./src/js/main.js', './src/js/a.js'],
-//context
-    entry:{
-    	main:'./src/js/main.js',
-    	a:'./src/js/a.js'
-    },/ 
-	output:{
-		path:path.resolve(__dirname,'./dist/js'),
-		filename:'[name]-[chunkhash].js'
-		//	filename:'[name]-[hash].js'
-	},
-	plugins:[
-	new htmlWebpackPlugin({
-		template:'index.html'
-	})
-	]
-}
+// var path=require('path');
+// var htmlWebpackPlugin=require('html-webpack-plugin');//commonJS的写法
+// module.exports={
+// //	entry: ['./src/js/main.js', './src/js/a.js'],
+// //context
+//     entry:{
+//     	main:'./src/js/main.js',
+//     	a:'./src/js/a.js'
+//     },/ 
+// 	output:{
+// 		path:path.resolve(__dirname,'./dist/js'),
+// 		filename:'[name]-[chunkhash].js'
+// 		//	filename:'[name]-[hash].js'
+// 	},
+// 	plugins:[
+// 	new htmlWebpackPlugin({
+// 		template:'index.html'
+// 	})
+// 	]
+// }
 
-/*
- *[name] key的值
- *[hash] 本次打包的hash值
- * [chunkhash] 文件的hash值  类似于版本号 没有更改多的hash值不会改变
- * */
+// /*
+//  *[name] key的值
+//  *[hash] 本次打包的hash值
+//  * [chunkhash] 文件的hash值  类似于版本号 没有更改多的hash值不会改变
+//  * */
